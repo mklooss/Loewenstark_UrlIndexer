2f84ede5702cbc86a3a3b2f80ac60aea2967ada4
Mathis Klooß <mathis@mage-profis.de>
2017-08-16 08:39:33 +0200

    remove tabs from pr

--- app/code/community/Loewenstark/UrlIndexer/Helper/Data.php
+++ app/code/community/Loewenstark/UrlIndexer/Helper/Data.php
@@ -10,95 +10,95 @@
  * @license   https://github.com/mklooss/Loewenstark_UrlIndexer/blob/master/README.md
  */
 class Loewenstark_UrlIndexer_Helper_Data
-	extends Mage_Core_Helper_Abstract
+    extends Mage_Core_Helper_Abstract
 {
-	// isEnabled
-	const XML_PATH_IS_ENABLED = 'dev/index/enable';
-	// HideDisabledProducts
-	const XML_PATH_DISABLED_PRODUCTS = 'dev/index/disable_products';
-	// HideNotVisibileProducts
-	const XML_PATH_HIDE_PRODUCTS = 'dev/index/notvisible_products';
-	// HideNotVisibileProducts
-	const XML_PATH_DISABLED_CATEGORIES = 'dev/index/disable_categories';
-	// DoNotUseCategoryPathInProduct
-	const XML_PATH_DISABLE_CATEGORIE = 'catalog/seo/product_use_categories';
-	// OptimizeCategoriesLeftJoin
-	const XML_PATH_LEFTJOIN_CATEGORIE = 'dev/index/optimize_categorie_leftjoin';
+    // isEnabled
+    const XML_PATH_IS_ENABLED = 'dev/index/enable';
+    // HideDisabledProducts
+    const XML_PATH_DISABLED_PRODUCTS = 'dev/index/disable_products';
+    // HideNotVisibileProducts
+    const XML_PATH_HIDE_PRODUCTS = 'dev/index/notvisible_products';
+    // HideNotVisibileProducts
+    const XML_PATH_DISABLED_CATEGORIES = 'dev/index/disable_categories';
+    // DoNotUseCategoryPathInProduct
+    const XML_PATH_DISABLE_CATEGORIE = 'catalog/seo/product_use_categories';
+    // OptimizeCategoriesLeftJoin
+    const XML_PATH_LEFTJOIN_CATEGORIE = 'dev/index/optimize_categorie_leftjoin';
 
-	/**
-	 * is Modul enabled
-	 *
-	 * @param int $storeid
-	 * @return bool
-	 */
-	public function isEnabled($storeid = 0)
-	{
-		return Mage::getStoreConfigFlag(self::XML_PATH_IS_ENABLED, $storeid);
-	}
+    /**
+     * is Modul enabled
+     *
+     * @param int $storeid
+     * @return bool
+     */
+    public function isEnabled($storeid = 0)
+    {
+        return Mage::getStoreConfigFlag(self::XML_PATH_IS_ENABLED, $storeid);
+    }
 
-	/**
-	 * disabled products
-	 *
-	 * @param int $storeid
-	 * @return boolean
-	 */
-	public function HideDisabledProducts($storeid = 0)
-	{
-		return $this->_getConfigFlag(self::XML_PATH_DISABLED_PRODUCTS, $storeid);
-	}
+    /**
+     * disabled products
+     *
+     * @param int $storeid
+     * @return boolean
+     */
+    public function HideDisabledProducts($storeid = 0)
+    {
+        return $this->_getConfigFlag(self::XML_PATH_DISABLED_PRODUCTS, $storeid);
+    }
 
-	/**
-	 * disabled categories
-	 *
-	 * @param int $storeid
-	 * @return boolean
-	 */
-	public function HideDisabledCategories($storeid = 0)
-	{
-		return $this->_getConfigFlag(self::XML_PATH_DISABLED_CATEGORIES, $storeid);
-	}
+    /**
+     * disabled categories
+     *
+     * @param int $storeid
+     * @return boolean
+     */
+    public function HideDisabledCategories($storeid = 0)
+    {
+        return $this->_getConfigFlag(self::XML_PATH_DISABLED_CATEGORIES, $storeid);
+    }
 
-	/**
-	 *
-	 * @param int $storeid
-	 * @return bool
-	 */
-	public function HideNotVisibileProducts($storeid = 0)
-	{
-		return $this->_getConfigFlag(self::XML_PATH_HIDE_PRODUCTS, $storeid);
-	}
+    /**
+     *
+     * @param int $storeid
+     * @return bool
+     */
+    public function HideNotVisibileProducts($storeid = 0)
+    {
+        return $this->_getConfigFlag(self::XML_PATH_HIDE_PRODUCTS, $storeid);
+    }
 
-	/**
-	 *
-	 * @param int $storeid
-	 * @return bool
-	 */
-	public function OptimizeCategoriesLeftJoin($storeid = 0)
-	{
-		return !$this->_getConfigFlag(self::XML_PATH_LEFTJOIN_CATEGORIE, $storeid);
-	}
+    /**
+     *
+     * @param int $storeid
+     * @return bool
+     */
+    public function OptimizeCategoriesLeftJoin($storeid = 0)
+    {
+        return !$this->_getConfigFlag(self::XML_PATH_LEFTJOIN_CATEGORIE, $storeid);
+    }
 
-	/**
-	 *
-	 * @param int $storeid
-	 * @return bool
-	 */
-	public function DoNotUseCategoryPathInProduct($storeid = 0)
-	{
-		return !$this->_getConfigFlag(self::XML_PATH_DISABLE_CATEGORIE, $storeid);
-	}
+    /**
+     *
+     * @param int $storeid
+     * @return bool
+     */
+    public function DoNotUseCategoryPathInProduct($storeid = 0)
+    {
+        return !$this->_getConfigFlag(self::XML_PATH_DISABLE_CATEGORIE, $storeid);
+    }
 
-	/**
-	 * Mage::getStoreConfigFlag
-	 *
-	 * @param string $xmlPath
-	 * @param int $storeid
-	 */
-	protected function _getConfigFlag($xmlPath, $storeid = 0)
-	{
-		if ($this->isEnabled()) {
-			return Mage::getStoreConfigFlag($xmlPath, $storeid);
-		}
-		return false;
-	}
+    /**
+     * Mage::getStoreConfigFlag
+     *
+     * @param string $xmlPath
+     * @param int $storeid
+     */
+    protected function _getConfigFlag($xmlPath, $storeid = 0)
+    {
+        if ($this->isEnabled()) {
+            return Mage::getStoreConfigFlag($xmlPath, $storeid);
+        }
+        return false;
+    }
 }
--- app/code/community/Loewenstark/UrlIndexer/Model/Url.php
+++ app/code/community/Loewenstark/UrlIndexer/Model/Url.php
@@ -10,123 +10,123 @@
  * @license   https://github.com/mklooss/Loewenstark_UrlIndexer/blob/master/README.md
  */
 class Loewenstark_UrlIndexer_Model_Url
-	extends Mage_Catalog_Model_Url
+    extends Mage_Catalog_Model_Url
 {
-	CONST XML_PATH_DISABLE_CATEGORIE = 'catalog/seo_product/use_categories';
+    CONST XML_PATH_DISABLE_CATEGORIE = 'catalog/seo_product/use_categories';
 
-	protected $_urlKey = false;
+    protected $_urlKey = false;
 
-	/**
-	 * Get unique product request path
-	 *
-	 * @param   Varien_Object $product
-	 * @param   Varien_Object $category
-	 * @return  string
-	 */
-	public function getProductRequestPath($product, $category)
-	{
-		$url = parent::getProductRequestPath($product, $category);
-		$this->_urlKey = false;
-		$suffix = $this->getProductUrlSuffix($category->getStoreId());
-		$urlKey = basename($url, $suffix); // get current url key
-		if ($this->_helper()->isEnabled() && $category->getLevel() == 1 && ($product->getUrlKey() == '' || $urlKey != $product->getUrlKey())) {
-			$this->_urlKey = $urlKey;
-			$product->setUrlKey($urlKey);
-			$this->getResource()->saveProductAttribute($product, 'url_key');
-		}
-		return $url;
-	}
+    /**
+     * Get unique product request path
+     *
+     * @param   Varien_Object $product
+     * @param   Varien_Object $category
+     * @return  string
+     */
+    public function getProductRequestPath($product, $category)
+    {
+        $url = parent::getProductRequestPath($product, $category);
+        $this->_urlKey = false;
+        $suffix = $this->getProductUrlSuffix($category->getStoreId());
+        $urlKey = basename($url, $suffix); // get current url key
+        if ($this->_helper()->isEnabled() && $category->getLevel() == 1 && ($product->getUrlKey() == '' || $urlKey != $product->getUrlKey())) {
+            $this->_urlKey = $urlKey;
+            $product->setUrlKey($urlKey);
+            $this->getResource()->saveProductAttribute($product, 'url_key');
+        }
+        return $url;
+    }
 
-	/**
-	 * refresh url rewrites by product ids
-	 *
-	 * @param array $productIds
-	 * @param null|int $store_id
-	 * @return Loewenstark_UrlIndexer_Model_Url
-	 */
-	public function refreshProductRewriteByIds($productIds, $store_id = null)
-	{
-		$stores = array();
-		if (is_null($store_id)) {
-			$stores = $this->getStores();
-		} else {
-			$stores = array((int)$store_id => $this->getStores($store_id));
-		}
-		foreach ($stores as $storeId => $store) {
-			$storeRootCategoryId = $store->getRootCategoryId();
-			$storeRootCategory = $this->getResource()->getCategory($storeRootCategoryId, $storeId);
-			$process = true;
-			$lastEntityId = 0;
-			while ($process == true) {
-				$products = $this->getResource()->getProductsByIds($productIds, $storeId, $lastEntityId);
-				if (!$products) {
-					$process = false;
-					break;
-				}
-				foreach ($products as $product) {
-					$categories = $this->getResource()->getCategories($product->getCategoryIds(), $storeId);
-					if (!isset($categories[$storeRootCategoryId])) {
-						$categories[$storeRootCategoryId] = $storeRootCategory;
-					}
-					foreach ($categories as $category) {
-						$this->_refreshProductRewrite($product, $category);
-					}
-				}
-			}
-		}
-		return $this;
-	}
+    /**
+     * refresh url rewrites by product ids
+     *
+     * @param array $productIds
+     * @param null|int $store_id
+     * @return Loewenstark_UrlIndexer_Model_Url
+     */
+    public function refreshProductRewriteByIds($productIds, $store_id = null)
+    {
+        $stores = array();
+        if (is_null($store_id)) {
+            $stores = $this->getStores();
+        } else {
+            $stores = array((int)$store_id => $this->getStores($store_id));
+        }
+        foreach ($stores as $storeId => $store) {
+            $storeRootCategoryId = $store->getRootCategoryId();
+            $storeRootCategory = $this->getResource()->getCategory($storeRootCategoryId, $storeId);
+            $process = true;
+            $lastEntityId = 0;
+            while ($process == true) {
+                $products = $this->getResource()->getProductsByIds($productIds, $storeId, $lastEntityId);
+                if (!$products) {
+                    $process = false;
+                    break;
+                }
+                foreach ($products as $product) {
+                    $categories = $this->getResource()->getCategories($product->getCategoryIds(), $storeId);
+                    if (!isset($categories[$storeRootCategoryId])) {
+                        $categories[$storeRootCategoryId] = $storeRootCategory;
+                    }
+                    foreach ($categories as $category) {
+                        $this->_refreshProductRewrite($product, $category);
+                    }
+                }
+            }
+        }
+        return $this;
+    }
 
-	/**
-	 * Refresh product rewrite
-	 *
-	 * @param Varien_Object $product
-	 * @param Varien_Object $category
-	 * @return Mage_Catalog_Model_Url
-	 */
-	protected function _refreshProductRewrite(Varien_Object $product, Varien_Object $category)
-	{
-		if ($category->getId() == $category->getPath()) {
-			return $this;
-		}
-		parent::_refreshProductRewrite($product, $category);
-		if ($this->_helper()->isEnabled() && $this->_urlKey && $this->_urlKey != $product->getUrlKey()) {
-			$product->setUrlKey($this->_urlKey);
-			$this->getResource()->saveProductAttribute($product, 'url_key');
-		}
-		return $this;
-	}
+    /**
+     * Refresh product rewrite
+     *
+     * @param Varien_Object $product
+     * @param Varien_Object $category
+     * @return Mage_Catalog_Model_Url
+     */
+    protected function _refreshProductRewrite(Varien_Object $product, Varien_Object $category)
+    {
+        if ($category->getId() == $category->getPath()) {
+            return $this;
+        }
+        parent::_refreshProductRewrite($product, $category);
+        if ($this->_helper()->isEnabled() && $this->_urlKey && $this->_urlKey != $product->getUrlKey()) {
+            $product->setUrlKey($this->_urlKey);
+            $this->getResource()->saveProductAttribute($product, 'url_key');
+        }
+        return $this;
+    }
 
-	/**
-	 * Refresh products for category
-	 *
-	 * @param Varien_Object $category
-	 * @return Mage_Catalog_Model_Url
-	 */
-	protected function _refreshCategoryProductRewrites(Varien_Object $category)
-	{
-		if (! $this->_helper()->DoNotUseCategoryPathInProduct($category->getStoreId())) {
-			return parent::_refreshCategoryProductRewrites($category);
-		}
-		return $this;
-	}
+    /**
+     * Refresh products for category
+     *
+     * @param Varien_Object $category
+     * @return Mage_Catalog_Model_Url
+     */
+    protected function _refreshCategoryProductRewrites(Varien_Object $category)
+    {
+        if (! $this->_helper()->DoNotUseCategoryPathInProduct($category->getStoreId())) {
+            return parent::_refreshCategoryProductRewrites($category);
+        }
+        return $this;
+    }
 
-	/**
-	 * Retrieve resource model, just for phpDoc :)
-	 *
-	 * @return Loewenstark_UrlIndexer_Model_Resource_Url
-	 */
-	public function getResource()
-	{
-		return parent::getResource();
-	}
+    /**
+     * Retrieve resource model, just for phpDoc :)
+     *
+     * @return Loewenstark_UrlIndexer_Model_Resource_Url
+     */
+    public function getResource()
+    {
+        return parent::getResource();
+    }
 
-	/**
-	 *
-	 * @return Loewenstark_UrlIndexer_Helper_Data
-	 */
-	protected function _helper()
-	{
-		return Mage::helper('urlindexer');
-	}
+    /**
+     *
+     * @return Loewenstark_UrlIndexer_Helper_Data
+     */
+    protected function _helper()
+    {
+        return Mage::helper('urlindexer');
+    }
 }
